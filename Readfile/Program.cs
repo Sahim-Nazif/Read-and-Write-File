b3c1525610ffbbfd6573269dddfa54e2f676e293
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-09-06 16:41:02 -0400

    Add project files.

---
+++ Readfile/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Readfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                //pass the file path and file name to the streamReader constructor.
+                StreamReader sr = new StreamReader("WriteTest.txt");
+
+                String line;
+                //read the first line
+                line = sr.ReadLine();
+                //continue to read until you reach the end of file
+                while (line != null)
+                {
+                    //write the first line to console
+                    Console.WriteLine(line);
+                    //Readfile the next line
+                    line = sr.ReadLine();
+
+                }
+                sr.Close();
+                Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception:" + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Now executing the finally block");
+                Console.ReadKey();
+            }
+        }
+    }
+}
